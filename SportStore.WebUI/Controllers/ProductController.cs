ff0f6f78ce8639dac61aee0cadb3c3a50489ce3e
YuriiTrofimenko <tyaa@ukr.net>
2018-10-23 17:53:42 +0300

    pagination step 1

--- SportStore.WebUI/Controllers/ProductController.cs
+++ SportStore.WebUI/Controllers/ProductController.cs
@@ -11,27 +11,40 @@ namespace SportStore.WebUI.Controllers
     public class ProductController : Controller
     {
         private IProductRepository rep;
+        private int pageSize = 2;
         public ProductController(IProductRepository _rep) {
             rep = _rep;
         }
         // GET: Product
-        public ViewResult List()
+        /*public ViewResult List()
         {
             //1
             //return View();
 
             //2
-            /*IQueryable<Product> products =
+            IQueryable<Product> products =
                 new List<Product>() {
                     new Product() {Name = "N1", Price = 10 }
                     , new Product() {Name = "N2", Price = 50 }
                     , new Product() {Name = "N3", Price = 5 }
                 }.AsQueryable();
-            return View(products);*/
+            return View(products);
 
             //3
             //return View(rep.Products);
             return View(rep.Products.ToList());
+        }*/
+
+        public ViewResult List(int page = 1)
+        {
+            return View(
+                rep.Products
+                .OrderBy(p => p.Category)
+                .ThenBy(p => p.Price)
+                .Skip((page * pageSize) - pageSize)
+                .Take(pageSize)
+                .ToList()
+            );
         }
     }
 }
